60aad8e356f2835cff91ad9dfa7ffc01b48c7a0d
Victor <vvanderhulst@axveco.com>
2019-06-05 09:13:16 +0200

    migration changed to EURO

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,9 +4,9 @@ let BN = web3.utils.BN
 
 var UBIVault = artifacts.require("UBIVault");
 
-let initialAB = 700 // 700 dollar cent = 7$
+let initialAB = 700 // 700 euro cent = €7
 let initialMinimumPeriod = 120 //  2 minutes, should be 604800 (60*60*24*7) in production
-let initialWeiToDollarCent = new BN(60332188286475) // ether to dollar exchange rate in cents to wei
+let initialWeiToEuroCent = new BN(46294153048469) // ether to euro exchange rate in cents to wei
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(UBIVault, initialAB, initialMinimumPeriod, initialWeiToDollarCent, accounts[0])
+  deployer.deploy(UBIVault, initialAB, initialMinimumPeriod, initialWeiToEuroCent, accounts[0])
 }
